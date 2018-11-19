0ded72561b61b1c675d90bc67272bd6ff8728398
Iago Leal <iago.lealf@gmail.com>
2018-11-19 00:45:15 -0200

    New herbestluftwm theme

--- .config/herbstluftwm/theme.sh
+++ .config/herbstluftwm/theme.sh
@@ -8,27 +8,27 @@ hc set frame_border_normal_color '#101010'
 hc set frame_bg_normal_color     '#565656'
 hc set frame_bg_active_color     '#345F0C'
 hc set frame_border_width 1
-hc set always_show_frame 1
+hc set always_show_frame 0
 hc set frame_bg_transparent 1
-hc set frame_transparent_width 2
-hc set frame_gap 2  
+hc set frame_transparent_width 0
+hc set frame_gap 5
 
-hc attr theme.active.color       '#34770C'
+hc attr theme.active.color        #34FF0C'
 hc attr theme.normal.color       '#454545'
 hc attr theme.urgent.color orange
 hc attr theme.inner_width 1
 hc attr theme.inner_color black
-hc attr theme.border_width 1
+hc attr theme.border_width 2
 hc attr theme.floating.border_width 4
 hc attr theme.floating.outer_width 1
 hc attr theme.floating.outer_color black
-hc attr theme.active.inner_color '#3E4A00'
-hc attr theme.active.outer_color '#3E4A00'
+hc attr theme.active.inner_color '#3EFF00'
+hc attr theme.active.outer_color '#3EFF00'
 hc attr theme.background_color   '#141414'
 
-hc set window_gap 0
+hc set window_gap 3
 hc set frame_padding 0
-hc set smart_window_surroundings 0
+hc set smart_window_surroundings 1
 hc set smart_frame_surroundings 1
 hc set mouse_recenter_gap 0
 
