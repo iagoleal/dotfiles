e87e7b2bbbbec2c8cedf392f7000a7412e9844cb
Iago Leal <iago.lealf@gmail.com>
2018-11-20 00:53:16 -0200

    Mopidy is now running alongside mpd. Added some binds and aliases to use it

--- .config/herbstluftwm/modes/Launcher.sh
+++ .config/herbstluftwm/modes/Launcher.sh
@@ -4,15 +4,16 @@ herbstclient keyunbind --all
 
 normalMode="$HOME/.config/herbstluftwm/keybindings.sh"
 
-herbstclient keybind m chain , spawn termite -e ncmpcpp , spawn sh "$normalMode"
-herbstclient keybind h chain , spawn termite -e htop    , spawn sh "$normalMode"
-herbstclient keybind w chain , spawn wicd-client        , spawn sh "$normalMode"
-herbstclient keybind f chain , spawn firefox            , spawn sh "$normalMode"
-herbstclient keybind q chain , spawn qutebrowser        , spawn sh "$normalMode"
-herbstclient keybind p chain , spawn zathura            , spawn sh "$normalMode"
-herbstclient keybind t chain , spawn transmission-gtk   , spawn sh "$normalMode"
-herbstclient keybind a chain , spawn anki               , spawn sh "$normalMode"
-herbstclient keybind e chain , spawn pcmanfm            , spawn sh "$normalMode"
+herbstclient keybind m chain , spawn termite -e ncmpcpp           , spawn sh "$normalMode"
+herbstclient keybind s chain , spawn termite -e "ncmpcpp -p 6601" , spawn sh "$normalMode"
+herbstclient keybind h chain , spawn termite -e htop              , spawn sh "$normalMode"
+herbstclient keybind w chain , spawn wicd-client                  , spawn sh "$normalMode"
+herbstclient keybind f chain , spawn firefox                      , spawn sh "$normalMode"
+herbstclient keybind q chain , spawn qutebrowser                  , spawn sh "$normalMode"
+herbstclient keybind p chain , spawn zathura                      , spawn sh "$normalMode"
+herbstclient keybind t chain , spawn transmission-gtk             , spawn sh "$normalMode"
+herbstclient keybind a chain , spawn anki                         , spawn sh "$normalMode"
+herbstclient keybind e chain , spawn pcmanfm                      , spawn sh "$normalMode"
 
 herbstclient keybind Mod4-Return chain , spawn termite , spawn sh "$normalMode"
 
