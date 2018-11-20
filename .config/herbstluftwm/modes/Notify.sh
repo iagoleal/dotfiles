0b785967c2417122191f160d470b37339824289a
Iago Leal <iago.lealf@gmail.com>
2018-11-20 16:17:25 -0200

    Some revamping of my notification system

--- .config/herbstluftwm/modes/Notify.sh
+++ .config/herbstluftwm/modes/Notify.sh
@@ -4,7 +4,7 @@ herbstclient keyunbind --all
 
 normalMode="$HOME/.config/herbstluftwm/keybindings.sh"
 
-herbstclient keybind t chain , spawn notify-send "$(date)" , spawn sh "$normalMode"
+herbstclient keybind t chain , spawn notify-send "$($HOME/.bin/getdate)"  , spawn sh "$normalMode"
 
 herbstclient keybind Escape spawn bash "$normalMode"
 
