cb01d9cbd69e907bfa629ff3e3929d48d0055334
Iago Leal <iago.lealf@gmail.com>
2019-01-03 22:19:22 -0200

    Herbstluftwm: Some updates on notifications mode

--- .config/herbstluftwm/modes/Notify.sh
+++ .config/herbstluftwm/modes/Notify.sh
@@ -4,8 +4,9 @@ herbstclient keyunbind --all
 
 normalMode="$HOME/.config/herbstluftwm/keybindings.sh"
 
-herbstclient keybind t chain , spawn "$HOME/.bin/notify_script" "-s" "$HOME/.bin/getdate" , spawn sh "$normalMode"
-herbstclient keybind c chain , spawn "$HOME/.bin/notify_script" "-s" "$HOME/.bin/getcal" , spawn sh "$normalMode"
+herbstclient keybind q chain , spawn "$HOME/.bin/notify" "-s" "$HOME/.bin/getinfo" , spawn sh "$normalMode"
+herbstclient keybind t chain , spawn "$HOME/.bin/notify" "-s" "$HOME/.bin/getdate" , spawn sh "$normalMode"
+herbstclient keybind c chain , spawn "$HOME/.bin/notify" "-s" "$HOME/.bin/getcal" , spawn sh "$normalMode"
 
 herbstclient keybind Escape spawn bash "$normalMode"
 
