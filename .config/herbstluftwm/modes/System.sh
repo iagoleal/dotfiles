ea2b03933210085648eda87af73081267557ba04
Iago Leal <iago.lealf@gmail.com>
2019-01-17 13:09:36 -0200

    System: logout, reboot, shutdown etc. is now controled by a script

--- .config/herbstluftwm/modes/System.sh
+++ .config/herbstluftwm/modes/System.sh
@@ -5,12 +5,17 @@ herbstclient keyunbind --all
 normalMode="$HOME/.config/herbstluftwm/keybindings.sh"
 
 
-herbstclient keybind 1 chain , spawn i3lock -c '#000000'                        , spawn sh "$normalMode"
-herbstclient keybind 2 chain , spawn i3lock -c '#000000' && systemctl suspend   , spawn sh "$normalMode"
-herbstclient keybind 3 chain , quit                                             , spawn sh "$normalMode"
-herbstclient keybind 4 chain , spawn i3lock -c '#000000' && systemctl hibernate , spawn sh "$normalMode"
-herbstclient keybind 5 chain , spawn systemctl reboot                           , spawn sh "$normalMode"
-herbstclient keybind 6 chain , spawn systemctl poweroff -i                      , spawn sh "$normalMode"
+herbstclient keybind 1 chain , spawn "$HOME/.bin/system" "lock"      \
+                             , spawn sh "$normalMode"
+herbstclient keybind 2 chain , spawn "$HOME/.bin/system" "suspend"
+                             , spawn sh "$normalMode"
+herbstclient keybind 3 chain , spawn "$HOME/.bin/system" "logout"
+                             , spawn sh "$normalMode"
+herbstclient keybind 4 chain , spawn "$HOME/.bin/system" "hibernate"
+                             , spawn sh "$normalMode"
+herbstclient keybind 5 chain , spawn "$HOME/.bin/system" "reboot"
+                             , spawn sh "$normalMode"
+herbstclient keybind 6 chain , spawn "$HOME/.bin/system" "poweroff"
+                             , spawn sh "$normalMode"
 
 herbstclient keybind Escape spawn bash "$normalMode"
-
