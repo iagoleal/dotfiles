a4b658a6a5aebfad13506538b848e5626f84c7cc
Iago Leal <iago.lealf@gmail.com>
2019-02-06 18:03:00 -0200

    Scripts: added screenshot script

--- .config/herbstluftwm/modes/Printscreen.sh
+++ .config/herbstluftwm/modes/Printscreen.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 herbstclient keyunbind --all
 
 normalMode="$HOME/.config/herbstluftwm/keybindings.sh"
 screenshotsFolder="$HOME/Pictures/Screenshots"
 
-herbstclient keybind Print   chain , spawn import -window root "$screenshotsFolder/$(date +%Y-%m-%d_%H:%M:%S).png" , spawn sh "$normalMode"
+herbstclient keybind Print   chain , spawn "$HOME/.bin/printscreen"       , spawn sh "$normalMode"
 
-herbstclient keybind f       chain , spawn import -window root "$screenshotsFolder/$(date +%Y-%m-%d_%H:%M:%S).png" , spawn sh "$normalMode"
-herbstclient keybind Shift+f chain , spawn import -window root  png:- | xclip -selection c -t image/png           , spawn sh "$normalMode" 
-herbstclient keybind c       chain , spawn import "$screenshotsFolder/$(date +%Y-%m-%d_%H:%M:%S)_cropped.png"      , spawn sh "$normalMode"
-herbstclient keybind Shift+c chain , spawn import png:- | xclip -selection c -t image/png                         , spawn sh "$normalMode"
+herbstclient keybind f       chain , spawn "$HOME/.bin/printscreen"       , spawn sh "$normalMode"
+herbstclient keybind Shift+f chain , spawn "$HOME/.bin/printscreen" "-w"  , spawn sh "$normalMode"
+herbstclient keybind c       chain , spawn "$HOME/.bin/printscreen" "-c"  , spawn sh "$normalMode"
+herbstclient keybind Shift+c chain , spawn "$HOME/.bin/printscreen" "-cw" , spawn sh "$normalMode"
 
-herbstclient keybind Escape spawn bash "$normalMode"
+herbstclient keybind Escape spawn sh "$normalMode"
 
