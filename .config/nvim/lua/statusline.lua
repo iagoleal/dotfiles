072972ad18603283af712b56443b3d98aba01ba9
iago <iago.lealf@gmail.com>
2021-04-28 20:40:20 -0300

    nvim: adjustments on statusline

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,30 +1,47 @@
-function CursorMode()
-    local mode_map = {
-        ['n']  = 'Normal   ',
-        ['v']  = 'Visual   ',
-        ['V']  = 'V-Line   ',
-        [''] = 'V-Block  ',
-        ['i']  = 'Insert   ',
-        ['R']  = 'Replace  ',
-        ['Rv'] = 'V-replace',
-        ['c']  = 'Command  ',
-        ['t']  = 'Terminal ',
+local function highlight(mode, opts)
+  local hi = {"highlight " .. mode}
+  for k, v in pairs(opts) do
+    table.insert(hi, " " .. k .. "=" .. v)
+  end
+  vim.cmd(table.concat(hi))
+end
+
+local function cursor_mode()
+    local mode_name = {
+        ['n']  = 'Normal',
+        ['v']  = 'Visual',
+        ['V']  = 'V-Line',
+        [''] = 'V-Block',
+        ['i']  = 'Insert',
+        ['R']  = 'Replace',
+        ['Rv'] = 'V-Replace',
+        ['c']  = 'Command',
+        ['s']  = 'Select',
+        ['S']  = 'S-Line',
+        [''] = 'S-Block',
+        ['t']  = 'Terminal',
     }
+    local mode = vim.fn.mode()
 
-    return mode_map[vim.fn.mode()]
+    return "%-10( " .. (mode_name[mode] or mode) .. "%)"
 end
 
-function StatusLine()
-    local sl = table.concat(
-      { [[%-{luaeval("CursorMode()")}]]
-      , [[ %-t %-m %-r]]
-      , [[%{get(b:,'gitsigns_head','')}]]
-      , [[ %{get(b:,'gitsigns_status','')}]]
-      , [[ %= %y %c, %l:%L]]
-      })
-    return sl
+function statusline()
+  local sl = table.concat(
+    { "%#StatusMode#"
+    , cursor_mode()
+    , "%#StatusLine#"
+    , " %-t %-m %-r"
+    , "%= %c, %l:%L "
+    , "%#StatusLang#"
+    , " %{luaeval('vim.bo.filetype')} "
+    })
+  return sl
 end
+
+highlight("StatusMode", {gui = "bold", guifg = "Black", guibg = "White"})
+highlight("StatusLang", {gui = "bold", guifg = "Black", guibg = "#81A3FA"})
 
 vim.o.laststatus = 2
 vim.o.showmode   = false
-vim.o.statusline = StatusLine()
+vim.o.statusline = "%!luaeval('statusline()')"
