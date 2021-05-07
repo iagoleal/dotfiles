18d0b21d56fc1270181826cd6af45d5868f4a568
iago <iago.lealf@gmail.com>
2021-05-07 00:32:22 -0300

    nvim: stop lsp highlight on hold

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -37,8 +37,8 @@ local on_attach = function(client, bufnr)
   end
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
+  --[[ if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([=[
       hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
       hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
       hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
@@ -47,8 +47,8 @@ local on_attach = function(client, bufnr)
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
-  end
+    ]=], false)
+  end ]]
 end
 
 -- Lua
