e27fb33ea4d100ec14fd93085fa59e7b6fde8941
iago <iago.lealf@gmail.com>
2021-05-12 20:14:12 -0300

    nvim: lsp better keybindings and configs

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -18,11 +18,11 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'K',         '<Cmd>lua vim.lsp.buf.hover()<CR>')
   buf_set_keymap('n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>')
   buf_set_keymap('n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-  buf_set_keymap('n', '<space>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+  buf_set_keymap('n', '<leader>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
   buf_set_keymap('n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>')
   buf_set_keymap('n', '<space>e',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
   buf_set_keymap('n', '[d',        '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
@@ -31,9 +31,9 @@ local on_attach = function(client, bufnr)
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+    buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
   elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>")
+    buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.range_formatting()<CR>")
   end
 
   -- Set autocommands conditional on server_capabilities
@@ -53,41 +53,47 @@ end
 
 -- Lua
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = "/usr/share/lua-language-server"
-local sumneko_binary = "/bin/lua-language-server"
+do
+  local sumneko_root_path = "/usr/share/lua-language-server"
+  local sumneko_binary = "/bin/lua-language-server"
+  local path = vim.split(package.path, ';')
+  table.insert(path, "lua/?.lua")
+  table.insert(path, "lua/?/init.lua")
 
-nvim_lsp.sumneko_lua.setup {
-  on_attach = on_attach,
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim', 'love'},
-        disable = {'lowercase-global'}
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+  nvim_lsp.sumneko_lua.setup {
+    on_attach = on_attach,
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = path
         },
-        [vim.fn.expand'~/.luarocks/share/lua/5.4'] = true,
-        ['/usr/share/lua/5.4'] = true
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim', 'love'},
+          disable = {'lowercase-global'}
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.stdpath('config')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+          [vim.fn.expand'~/.luarocks/share/lua/5.4'] = true,
+          ['/usr/share/lua/5.4'] = true
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
       },
     },
-  },
-}
+  }
+end
 
 -- Haskell
 nvim_lsp.hls.setup{
@@ -98,7 +104,7 @@ nvim_lsp.hls.setup{
 nvim_lsp.julials.setup{
     on_attach = on_attach,
     autostart = true,
-    on_new_config = function(new_config,new_root_dir)
+    on_new_config = function(new_config, new_root_dir)
       local server_path = "/home/iago/.julia/packages/LanguageServer/y1ebo/src"
       local cmd = {
         "julia",
