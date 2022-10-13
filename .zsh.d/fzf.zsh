dbb02109868757a030c0e1c28d5146597e4c90e1
Iago Leal <iago.lealf@gmail.com>
2022-10-13 00:24:40 -0300

    (zsh): tune up fzf completion

--- .zsh.d/fzf.zsh
+++ .zsh.d/fzf.zsh
@@ -33,3 +33,31 @@ export FZF_DEFAULT_OPTS='-m'
 if command -v rg &> /dev/null; then
   export FZF_DEFAULT_COMMAND='rg --files'
 fi
+
+# Git completion
+source /home/iago/.zsh.d/lib/fzf-git.sh
+
+# use fzf for general tab completion
+source $HOME/.zsh.d/lib/fzf-tab/fzf-tab.plugin.zsh
+
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' fzf-preview 'echo ${(P)word}'
+
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
+  'git diff --color=always $word'
+zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
+  'git log --color=always $word'
+zstyle ':fzf-tab:complete:git-help:*' fzf-preview \
+  'git help $word | bat -plman --color=always'
+zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+  'case "$group" in
+  "commit tag") git show --color=always $word ;;
+  *) git show --color=always $word  ;;
+  esac'
+zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+  'case "$group" in
+  "modified file") git diff --color=always $word ;;
+  "recent commit object name") git show --color=always $word ;;
+  *) git log --color=always $word ;;
+  esac'
