b5584725c04ff788992f35dc30e7d6ac39db56f7
Iago Leal <iago@iagoleal.com>
2023-09-08 02:47:29 -0300

    (pkgs): Move some packages from pacman to nix

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,31 +1,43 @@
 {
+  allowUnfree = true;
+
   packageOverrides = pkgs: with pkgs; {
     myPackages = pkgs.buildEnv {
       name = "my-packages";
 
       paths = [
-        # Programming environments
+        # ~ Programming environments ~ #
         idris2
         luajitPackages.fennel
         chez
         racket-minimal
 
-        # CLI tooling
+        # ~ CLI tooling ~ #
         fzf
         pistol
         ripgrep
-        tmux
         ueberzug
         w3m
         direnv     # Allows activation of of an environment per dir (e.g., auto nix-shell)
         nix-direnv # Better direnv integration with nix (includes caching)
 
+        # ~ Academic life ~ #
+        zotero
+        pandoc
 
-        # Language Servers
-        sumneko-lua-language-server
-        pyright
-        ltex-ls
+        # ~ Language Servers ~ #
+        sumneko-lua-language-server  # Lua
+        pyright                      # Python
+        ltex-ls                      # Text: Latex, Markdown, txt
+        nodePackages.bash-language-server
+
+        # ~ Graphics ~ #
+        darktable
+        colorpicker
       ];
+
+      pathsToLink = [ "/share" "/bin" ];
+      extraOutputsToInstall = [ "man" "doc" ];
     };
   };
 }
